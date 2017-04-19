e07647cd21e2b4854f99554b02e1cfbb79a976af
vanhakobyan <vanhakobyan1996@gmail.com>
2017-04-19 15:43:07 +0400

    add Room Controller :one:

---
+++ APIforWPF/APIforWPF/Controllers/RoomController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIforWPF.Controllers
+{
+    public class RoomController : ApiController
+    {
+        // GET: api/Room
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Room/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Room
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Room/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Room/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
